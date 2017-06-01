8374bf2da42f01adf781f8f96e7c55d379b00437
Richard Saville <richardasaville@gmail.com>
2017-06-01 14:04:58 +1000

    added in react-materialize and react-ui, created mock up dryer card

---
+++ src/MyAwesomeReactComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const style = {
+  margin: 12,
+};
+
+const MyAwesomeReactComponent = () => (
+  <RaisedButton label="Primary" primary={true} style={style} />
+);
+
+export default MyAwesomeReactComponent;
